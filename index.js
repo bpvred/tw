6a443fb1b9666175a57449b392b417dcbd81d201
bpvred <ddusardom@gmail.com>
2026-07-26 08:21:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -368,3 +368,14 @@ registrar();
 
 
 client.login(TOKEN);
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot online!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor web ativo");
+});
